d14525661a54cb605807d415e5d2adf8c20ab939
Alejandro Ambrosio <67438573+alejandro342@users.noreply.github.com>
2024-01-03 18:54:48 -0600

    notificaciones
    Se agrego un  Badge para mostrar notificaciones.
    
    Co-Authored-By: Alejandro Ambrosio <67438573+alejandro342@users.noreply.github.com>

--- app/src/main/java/com/alejandro_dev_links/bottomnavbar/presentation/mycomponents/bottombar/components/UserBottomBarItem.kt
+++ app/src/main/java/com/alejandro_dev_links/bottomnavbar/presentation/mycomponents/bottombar/components/UserBottomBarItem.kt
@@ -1,15 +1,17 @@
 package com.alejandro_dev_links.bottomnavbar.presentation.mycomponents.bottombar.components
 
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BadgedBox
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Icon
 import androidx.compose.material.LocalContentColor
 import androidx.compose.material.Text
+import androidx.compose.material3.Badge
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
@@ -17,16 +19,30 @@ import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import com.alejandro_dev_links.bottomnavbar.presentation.navigation.screen.user.UserScreen
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RowScope.UserBottomBarItem(
     screen: UserScreen,
     currentDestination: NavDestination?,
     navController: NavHostController
 ) {
-    BottomNavigationItem(modifier = Modifier
-        .clip(RoundedCornerShape(topStart = 15.dp, topEnd = 15.dp)),
+    BottomNavigationItem(modifier = Modifier,
         label = { Text(text = screen.title) },
-        icon = { Icon(imageVector = screen.icon, contentDescription = "icono") },
+        alwaysShowLabel = false,
+        icon = {
+            BadgedBox(modifier = Modifier.padding(start = 25.dp), badge = {
+                if (screen.badgeCount != null) {
+                    Badge {
+                        Text(text = screen.badgeCount.toString())
+                    }
+                } else if (screen.hasNews) {
+                    Badge()
+                }
+            }) {
+
+            }
+            Icon(imageVector = screen.icon, contentDescription = "icono")
+        },
         selected = currentDestination?.hierarchy?.any {
             it.route == screen.route
         } == true,
--- app/src/main/java/com/alejandro_dev_links/bottomnavbar/presentation/navigation/screen/user/UserScreen.kt
+++ app/src/main/java/com/alejandro_dev_links/bottomnavbar/presentation/navigation/screen/user/UserScreen.kt
@@ -7,30 +7,42 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.ui.graphics.vector.ImageVector
 
-sealed class UserScreen(val route: String, val title: String, val icon: ImageVector) {
+sealed class UserScreen(
+    val route: String,
+    val title: String,
+    val icon: ImageVector,
+    val hasNews: Boolean,
+    val badgeCount: Int? = null
+) {
 
 
     object Home : UserScreen(
         route = "user/home",
         title = "Home",
-        icon = Icons.Default.Home
+        icon = Icons.Default.Home,
+        hasNews = false,
+        badgeCount = null
     )
 
     object Favorites : UserScreen(
         route = "user/favorites",
         title = "Favorites",
-        icon = Icons.Outlined.FavoriteBorder
+        icon = Icons.Outlined.FavoriteBorder,
+        hasNews = true,
+        badgeCount = 45
     )
 
     object Profile : UserScreen(
         route = "user/profile",
         title = "Profile",
-        icon = Icons.Default.Person
+        icon = Icons.Default.Person,
+        hasNews = false
     )
 
     object Settings : UserScreen(
         route = "user/settings",
         title = "Settings",
-        icon = Icons.Default.Settings
+        icon = Icons.Default.Settings,
+        hasNews = true
     )
 }
